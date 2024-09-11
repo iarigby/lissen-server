99aeed0a9576bffea20fbec4e3bbf94d6c4ea4c6
ia rigby <marvinzem@gmail.com>
2024-09-11 22:19:22 +0400

    enable new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DownloaderModule } from './downloader/downloader.module';
+import { ArticleModule } from './article/article.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [DownloaderModule, ArticleModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
